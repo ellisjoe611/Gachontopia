ff824e59ef340c54e0fa64534b188a92e41deb8c
Jong-Hyuk Ellis Cho <ellisjoe611@gmail.com>
2018-05-19 13:40:26 +0900

    updated SignupActivity.java

--- Topia/app/src/main/java/com/example/ellis/topia/SignupActivity.java
+++ Topia/app/src/main/java/com/example/ellis/topia/SignupActivity.java
@@ -10,20 +10,31 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-public class SignupActivity extends AppCompatActivity implements View.OnClickListener {
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
 
-    String[] district = {"강남구", "강동구", "강북구", "강서구", "관악구", "광진구", "구로구", "금천구",
-            "노원구", "도봉구", "동대문구", "동작구", "마포구", "서대문구", "서초구", "성동구", "성북구",
-            "송파구", "양천구", "영등포구", "용산구", "은평구", "종로구", "중구", "중랑구"};
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class SignupActivity extends AppCompatActivity {
+
+    String[] district = {"gangnam", "gangdong", "gangbook", "gangseo", "kwanak", "kwangjin", "guro", "geumcheon",
+            "nowon", "dobong", "dongdaemoon", "dongjak", "mapo", "seodaemoon", "seocho", "seongdong", "seongbook",
+            "songpa", "yangcheon", "yongdeungpo", "yongsan", "ennpyeong", "jongro", "joonggu", "joonglang"};
     EditText editText_newPhone, editText_newPW, editText_checkPW, editText_newAddress;
-    String selectedDistrict;
     Spinner spinner_newDistrict;
     Button button_createAccount;
+
+    String phone, password, home_area, home_addr; //DB에 있는 변수 이름과 동일
+    String type = "client"; //DB에 있는 변수 이름과 동일, 기본적인 회원 type = "client"
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
         editText_newPhone = findViewById(R.id.editText_newPhone);
         editText_newPW = findViewById(R.id.editText_newPW);
         editText_checkPW = findViewById(R.id.editText_checkPW);
@@ -39,50 +50,80 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
         spinner_newDistrict.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                selectedDistrict = district[position];
+                home_area = district[position]; //지역 이름을 DB명의 string에다가 기입
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-                selectedDistrict = "";
+                home_area = "";
             }
         });
 
-        button_createAccount.setOnClickListener(this);
+        //나머지 DB명의 값들을 기입
+        phone = editText_newPhone.getText().toString();
+        password = editText_newPW.getText().toString();
+        home_addr = editText_newAddress.getText().toString();
+
+
+        button_createAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(foundEmpty()){
+                    Toast.makeText(SignupActivity.this, "모든 부분을 채워주세요.", Toast.LENGTH_SHORT).show();
+                }
+                else if (!(editText_newPW.getText().toString().equals(editText_checkPW.getText().toString()))) {
+                    Toast.makeText(SignupActivity.this, "비밀번호를 다르게 입력하셨습니다.", Toast.LENGTH_SHORT).show();
+                }
+                else if(editText_newPW.getText().toString().length() < 5){
+                    Toast.makeText(SignupActivity.this, "PW는 5글자 이상 가능합니다.", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    //계정 추가 하기 프로세스 시작
+                    Response.Listener<String> responseListener = new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            try{
+                                //중요: JSON을 불러온다
+                                JSONObject jsonResponse = new JSONObject(response);
+                                boolean success = jsonResponse.getBoolean("success");   //변수 이름: "success" (true / false)
+                                if(success){
+                                    Toast.makeText(SignupActivity.this, "회원가입 성공", Toast.LENGTH_SHORT).show();
+                                    finish();
+                                }else{
+                                    Toast.makeText(SignupActivity.this, "Error: Failed to create account...", Toast.LENGTH_SHORT).show();
+                                }
+                            }catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    };
+
+                    SignupRequest signupRequest = new SignupRequest(phone, password, type, home_area, home_addr, responseListener);
+                    RequestQueue queue = Volley.newRequestQueue(SignupActivity.this);
+                    queue.add(signupRequest);
+
+                    //계정 추가 하기 프로세스 끝
+                }
+            }
+        });
     }
 
-    /**
-     * Called when a view has been clicked.
-     *
-     * @param v The view that was clicked.
-     */
-    @Override
-    public void onClick(View v) {
-        if(foundEmpty()){
-            Toast.makeText(this, "Error: Enter all inputs...", Toast.LENGTH_SHORT).show();
-        }
-        else{
-            //계정 추가 하기
-
-            
-        }
-    }
 
     public boolean foundEmpty(){
         boolean result = false;
         if(editText_newPhone.getText().toString().equalsIgnoreCase("")){
             result = true;
         }
-        if(editText_newPW.getText().toString().equalsIgnoreCase("")){
+        else if(editText_newPW.getText().toString().equalsIgnoreCase("")){
             result = true;
         }
-        if(editText_checkPW.getText().toString().equalsIgnoreCase("")){
+        else if(editText_checkPW.getText().toString().equalsIgnoreCase("")){
             result = true;
         }
-        if(editText_newAddress.getText().toString().equalsIgnoreCase("")){
+        else if(editText_newAddress.getText().toString().equalsIgnoreCase("")){
             result = true;
         }
-        if(selectedDistrict.equalsIgnoreCase("")){
+        else if(home_area.equalsIgnoreCase("")){
             result = true;
         }
         return result;
